8c621f8db0ced2d4800a3554ee1072de1d813e95
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-21 17:56:31 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -10,13 +10,13 @@
 permitroot=""       # Enable or Disable SSH root access - Options: yes, no
 port=""             # Set port for SSH service
 protocol=""         # Set SSH Protocol - Options: 1, 2
-allowusers=""       # Optional - Leave blank if uneeded - Set allowed Users - Usage: Enter space between users
+allowusers=""       # Optional - Leave blank if uneeded - Set allowed users - Usage: Enter space between users
 
 # Creates a temporary backup of sshd_config
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_bkp
 
 # Changes root permission, protocol and port configuration in sshd_config based on set variables
-cat /etc/ssh_config_bkp | sed 's/#PermitRootLogin yes/#PermitRootLogin '$permitroot'/'  | sed 's/#Protocol 2/Protocol '$protocol'/' | sed 's/#Port 22/Port '$port'/' > /etc/ssh/ssh_config
+cat /etc/ssh/sshd_config_bkp | sed 's/#PermitRootLogin yes/#PermitRootLogin '$permitroot'/'  | sed 's/#Protocol 2/Protocol '$protocol'/' | sed 's/#Port 22/Port '$port'/' > /etc/ssh/sshd_config
 
 # Runs and sets allowed users if variable is not left blank
 if [ -n "$allowusers" ]; then
@@ -24,7 +24,7 @@ if [ -n "$allowusers" ]; then
 fi
 
 # Removes ssh_config backup file
-rm -f /etc/ssh_config_bkp
+rm -f /etc/ssh/sshd_config_bkp
 
 # Restarts SSH service
 systemctl restart sshd
