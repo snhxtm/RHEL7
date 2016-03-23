88ef35b524c20eb7ba224ff9653d0ca3c872a628
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-23 16:18:02 +0100

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-$reposdir=""       # Example - /home/repos/centos/7/
+###############################################################################################################################
+#DESCRIPTION: An automated bash script for configuring and syncing a local repository on RHEL7/CentOS7.
+#USAGE: Edit the variables with your configuration and execute on RHEL7/CentOS7.
+#CREATED BY: William Thomas Bland.
+###############################################################################################################################
+
+$reposdir=""      # Example - /home/repos/centos/7/
 $mirror=""        # Example - rsync://ftp.plusline.de/CentOS/7/os_x64_86
 $reponame=""      # Example - Local CentOS 7 Repository
 $filename=""      # Example - local.repo
 
+# Install packages
 yum install createrepo rsync vsftpd -y
 
+# Create directory where repository will be downloaded
 mkdir -p $reposdir
 
+# Create repository
 createrepo $reposdir
 
+# Sync local repository with mirror
 rsync -rz --progress rsync://$mirror $localdir
-# ln -s /home/repos /var/ftp/
+
+# Create symbolic link between local repository and ftp directory
 $reposroot=$(echo $reposdir | cut -d/ -f2,3)
 ln -s $reposroot /var/ftp
-#test=$(echo $test | cut -d/ -f2,3)
 
+# Define repository in /etc/yum.repos.d/
 echo -e '[mylocalrepo] \
 name='$name' \
 baseurl=ftp://'$HOSTNAME'/centos/7 \
