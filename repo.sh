7ccd84eacbf6fa62315c098cc1c06b6c29a07caf
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-23 16:12:34 +0100

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-$repodir=""       # Example - /home/repos/centos/7/
+$reposdir=""       # Example - /home/repos/centos/7/
 $mirror=""        # Example - rsync://ftp.plusline.de/CentOS/7/os_x64_86
-$ftpdir=""        # Example - /var/ftp/public/centos
 $reponame=""      # Example - Local CentOS 7 Repository
 $filename=""      # Example - local.repo
 
 yum install createrepo rsync vsftpd -y
 
-mkdir -p $repodir
+mkdir -p $reposdir
 
-createrepo $repodir
+createrepo $reposdir
 
 rsync -rz --progress rsync://$mirror $localdir
-
-ln -s $ftpdir $localdir
+# ln -s /home/repos /var/ftp/
+$reposroot=$(echo $reposdir | cut -d/ -f2,3)
+ln -s $reposroot /var/ftp
+#test=$(echo $test | cut -d/ -f2,3)
 
 echo -e '[mylocalrepo] \
 name='$name' \
