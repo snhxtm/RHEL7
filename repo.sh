9c2f1372587a02fdfa789dff285234f396550461
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-23 14:37:48 +0100

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-yum install createrepo vsftpd -y
+$repodir=""       # Example - /home/repos/centos/7/
+$mirror=""        # Example - rsync://ftp.plusline.de/CentOS/7/os_x64_86
+$ftpdir=""        # Example - /var/ftp/public/centos
+$reponame=""      # Example - Local CentOS 7 Repository
+$filename=""      # Example - local.repo
 
-mkdir -p /home/repos/centos/7/packages
+yum install createrepo rsync vsftpd -y
 
-createrepo /home/repos/centos/7/packages
+mkdir -p $repodir
 
-rsync -rz --progress rsync://ct.mirror.garr.it/mirrors/CentOS/7/os/x86_64/ /home/repos/CentOS/7/
+createrepo $repodir
 
-ln -s /var/ftp/public/centos /home/repos/centos
+rsync -rz --progress rsync://$mirror $localdir
+
+ln -s $ftpdir $localdir
 
 echo -e '[mylocalrepo] \
-name=Local CentOS 7 Repository \
-baseurl=ftp://vm001.local/centos/7 \
-gpgcheck=0' > /etc/yum.repos.d/local.repo
+name='$name' \
+baseurl=ftp://'$HOSTNAME'/centos/7 \
+gpgcheck=0' > /etc/yum.repos.d/$filename
 
 exit 0
