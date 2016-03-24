0ff5aeee2077b6794a3b787dea8cb099468fe2c4
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-24 13:44:42 +0100

    Update and rename config.sh to post.sh

--- post.sh
+++ post.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 ###############################################################################################################################
-#DESCRIPTION: Script that installs extra packages repository, runs a full update and installs essential packages.
+#DESCRIPTION: A script that runs a full update and installs essential packages.
 #USAGE: Execute over a fresh minimal installation of RHEL7/CentOS7.
 #CREATED BY: William Thomas Bland.
 ###############################################################################################################################
-
-#add extra packages repository
-yum install epel-release -y
 
 #run full system update
 yum update -y
