1ebccfc729da3eed60c146e836d5230889d9edf8
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-26 13:36:32 +0100

    Update delvm.sh

--- delvm.sh
+++ delvm.sh
@@ -2,16 +2,18 @@
 
 ###############################################################################################################################
 #DESCRIPTION: An automated bash script that completely removes a selected virtual machine.
-#USAGE: Edit the variables with desired VM settings and execute on a KVM enabled RHEL7/CentOS7.
+#USAGE: Edit the variables with desired VM settings define variables as agruments and execute on a KVM enabled RHEL7/CentOS7.
 #CREATED BY: William Thomas Bland.
 ###############################################################################################################################
 
-$vm=""      # Virtual machine name
-$pool=""    # KVM images folder
+# Edit variables or define variables as agruments. Example - delv.sh vm pool
+$vm=$1      # Virtual machine name
+$pool=$2    # Pool name
+
+###############################################################################################################################
 
 virsh destroy $vm
 virsh undefine $vm
-virsh pool-refresh $pool
 virsh vol-delete --pool $pool $vm.img
 
 exit 0
