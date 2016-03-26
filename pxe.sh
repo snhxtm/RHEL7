180f5c46c7e6d2f902f1bf986a942ccfaf65d06a
William Thomas Bland <Halakor@users.noreply.github.com>
2016-03-26 17:39:14 +0100

    Update pxe.sh

--- pxe.sh
+++ pxe.sh
@@ -10,24 +10,25 @@ repodir=""    # Example - /var/www/html/centos/7/os/x_86_64
 
 ###############################################################################################################################
 
+# Install Packages
 yum install xinetd syslinux tftp-server -y
 
+# Enable tftp
 sed -i 's/^disable.yes$/^disable.no$/' /etc/xinetd.d/tftp
 
+# Copy syslinux files
 cd /usr/share/syslinux
 cp pxelinux.0 menu.c32 memdisk mboot.c32 chain.c32 /var/lib/tftpboot/
 
+# Copy pxeboot and initrd.img files
 cd $repodir/images/pxeboot/
 cp vmlinuz initrd.img /var/lib/tftpboot
 cd ~
 
+# Create config directory for pxe menu configuration
 mkdir -p /var/lib/tftpboot/pxelinux.cfg
 
-default menu.c32
-prompt 0
-timeout 300
-ONTIMEOUT local
-
+# Create and configure pxe menu configuration file
 echo -e 'default menu.c32
 prompt 0
 timeout 300
@@ -40,6 +41,7 @@ menu label ^1) Install CentOS 7
 kernel vmlinuz
 append initrd.img method=http://'$HOSTNAME'/centos/7/os/x86_64 devfs=nomount' > /var/lib/tftpboot/pexlinux.cfg/default
 
+# Start and enable Xinetd
 systemctl start xinetd
 systemctl enable xinetd
 
