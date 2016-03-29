99cb3d66311b79c1346e7c5f923aa4115b903f29
William Thomas Bland <william-thomas-bland@users.noreply.github.com>
2016-03-29 19:19:26 +0200

    Create sv_dns.sh

---
+++ sv_dns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############################################################################################################################
+#DESCRIPTION: An automated bash script for configuring a dns server on RHEL7/CentOS7.
+#USAGE: Edit the variables with desired configuration and execute on RHEL7/CentOS7.
+#CREATED BY: William Thomas Bland.
+###############################################################################################################################
+
+
+
+exit 0
